bc20ee9f12a59284a909f18d19a70538d6450df3
Prof-Paulinha <anapr_007@yahoo.com.br>
2022-10-10 14:46:24 -0300

    programa media

---
+++ ex01.py
@@ -0,0 +1,5 @@
+n1 = int(input('Digite a 1º nota: '))
+n2 = int(input('Digite a 2º nota: '))
+soma = n1 + n2
+media = soma / 2
+print('A soma de todas as notas é de {} e a média final é de {}'.format(soma, media))
